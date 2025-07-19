21fc50f555bf984a8a1223860191a748c261f840
SuperMartijn624 <martijnvandenbrand@outlook.com>
2025-07-19 18:20:53 +0200

    Fix potential 0 argument for generating random int in `StructurePlacement` #9

--- src/main/java/com/supermartijn642/formations/structure/StructurePlacement.java
+++ src/main/java/com/supermartijn642/formations/structure/StructurePlacement.java
@@ -84,7 +84,7 @@ public enum StructurePlacement implements StringRepresentable {
 
         int min = positions.stream().mapToInt(Triple::getRight).max().getAsInt();
         int max = positions.stream().mapToInt(Triple::getMiddle).min().getAsInt();
-        if(max - min - 2 < box.getYSpan())
+        if(max - min - 2 <= box.getYSpan())
             return null;
 
         return min + 1 + context.random().nextInt(max - min - box.getYSpan() - 2);
@@ -113,7 +113,7 @@ public enum StructurePlacement implements StringRepresentable {
 
         int min = positions.stream().mapToInt(Triple::getRight).max().getAsInt();
         int max = positions.stream().mapToInt(Triple::getMiddle).min().getAsInt();
-        if(max - min - 2 < box.getYSpan())
+        if(max - min - 2 <= box.getYSpan())
             return null;
 
         int height = min + 1 + context.random().nextInt(max - min - box.getYSpan() - 2);
@@ -175,7 +175,7 @@ public enum StructurePlacement implements StringRepresentable {
 
         int min = positions.stream().mapToInt(Triple::getRight).max().getAsInt();
         int max = positions.stream().mapToInt(Triple::getMiddle).min().getAsInt();
-        if(max - min - 2 < box.getYSpan())
+        if(max - min - 2 <= box.getYSpan())
             return null;
 
         int height = min + 1 + context.random().nextInt(max - min - box.getYSpan() - 2);
@@ -237,7 +237,7 @@ public enum StructurePlacement implements StringRepresentable {
 
         int min = positions.stream().mapToInt(Triple::getRight).max().getAsInt();
         int max = positions.stream().mapToInt(Triple::getMiddle).min().getAsInt();
-        if(max - min - 2 < box.getYSpan())
+        if(max - min - 2 <= box.getYSpan())
             return null;
 
         int ySpan = box.getYSpan();
@@ -299,7 +299,7 @@ public enum StructurePlacement implements StringRepresentable {
 
         int min = positions.stream().mapToInt(Triple::getRight).max().getAsInt();
         int max = positions.stream().mapToInt(Triple::getMiddle).min().getAsInt();
-        if(max - min - 2 < box.getYSpan())
+        if(max - min - 2 <= box.getYSpan())
             return null;
 
         int height = min + 1 + context.random().nextInt(max - min - box.getYSpan() - 2);
